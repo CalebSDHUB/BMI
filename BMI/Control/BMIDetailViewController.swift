da0e908374fae20947cd0760d4ba1fc8f45df150
Caleb Danielsen <caleb@lfpost.dk>
2021-10-01 08:39:35 +0200

    BMIDetailViewController tied together with the storyboard interface.

--- BMI/Control/BMIDetailViewController.swift
+++ BMI/Control/BMIDetailViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class BMIDetailViewController: UIViewController {
 
+    @IBOutlet weak var integerBMILabel: UILabel!
+    @IBOutlet weak var decimalBMILabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var BMIRangeLabel: UILabel!
+    @IBOutlet weak var CILabel: UILabel!
+    
     var user: User?
     
     override func viewDidLoad() {
@@ -17,4 +23,10 @@ class BMIDetailViewController: UIViewController {
         title = Constants.BMIDetailTitle
     }
 
+    @IBAction func shareButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func rateButton(_ sender: UIButton) {
+        
+    }
 }
--- BMI/Control/BMIViewController.swift
+++ BMI/Control/BMIViewController.swift
@@ -62,7 +62,8 @@ class BMIViewController: UIViewController {
     /// Calculate the BMI and the CI (Ponderal Index)
     @IBAction func calculateButton(_ sender: UIButton) {
         
-        /// We are able to force unwrap the four parameters because, in order to use this calculateButton function, the formValidation function makes sure that the parameters are instantiated. The fifth parameter (result) needs to be unwrapped safely.
+        /// We are able to force unwrap the four parameters because, in order to use this calculateButton function, the formValidation function makes sure that the parameters are instantiated.
+        /// The fifth parameter (result) needs to be unwrapped safely.
         guard let result = BMIEngine(weight!, height!).getResult() else { fatalError("Error: The Result object is not instantiated") }
         user = User(weight: weight!, height: height!, name: nameField.text!, gender: Gender(rawValue: gender!)!, result: result)
         
